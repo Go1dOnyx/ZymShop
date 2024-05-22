ef90b23acde1c61b7e5d23a7d395cb56cadaea20
Jesus Diaz <jdiazndca21@gmail.com>
2024-05-22 12:11:37 -0500

    Added new css classes for each nav component and updated their scripts

---
+++ src/app/nav/videos/videos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: '',
+    templateUrl: './videos.component.html',
+    styleUrl: './videos.component.css'
+})
+
+export class VideoComponent {
+
+}
