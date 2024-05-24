6d8dcc1a8476684de36392a3613a25c226c56011
Jesus Diaz <jdiazndca21@gmail.com>
2024-05-24 12:29:23 -0500

    Add a function for my button

--- src/app/nav/home/home.component.ts
+++ src/app/nav/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     templateUrl: './home.component.html',
@@ -6,5 +7,9 @@ import { Component } from "@angular/core";
 })
 
 export class HomeComponent {
+    constructor(private route: Router) {}
 
+    routeToPage(routeUrl:string): void {
+        this.route.navigate([`${routeUrl}`]);
+    }
 }
